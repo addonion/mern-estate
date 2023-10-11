028c1d3c5dc1f94bd1a7627be5835a07d89da273
addonion <addonion@gmail.com>
2023-10-11 13:49:08 +0500

    Create Sign Up API route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,22 @@
 import "dotenv/config";
-import expres from "express";
+import express from "express";
 import mongoose from "mongoose";
 
 import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGODB)
   .then(() => console.log("MONGODB connected"))
   .catch((err) => console.log(err));
 
-const app = expres();
+const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log(`Server on 3000 port`);
 });
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
